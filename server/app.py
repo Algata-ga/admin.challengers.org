13ca66a23e366c72cb8225a127e80a4781425a90
Alfred Jophy <alfredjophy@protonmail.com>
2022-03-22 19:10:33 +0530

    fixed cors

--- server/app.py
+++ server/app.py
@@ -12,7 +12,7 @@ app=Flask(__name__)
 app.secret_key = 'RANDOM STUFF'
 app.config['UPLOAD_PATH']= pathlib.Path.home()/'static'
 app.config['CORS_METHODS']=['GET']
-app.config['CORS_ORIGINS']=['cryptochallengers.org','cryptochallengers.preview.algata.in']
+app.config['CORS_ORIGINS']=['https://cryptochallengers.org','https://preview.algata.in']
 
 CORS(app,supports_credentials=True)
 
